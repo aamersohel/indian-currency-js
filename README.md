2d91418009ffcaa16c6243afc06f5e19f6b9cfc2
Aamer Sohel <aamersohel7@gmail.com>
2019-01-19 16:21:21 +0530

    International numbering system format support added

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Description
-Converts any number to string equivalent representation. Currently it supports Indian format, number range from -999999999 to 999999999.
+<p>Converts any number to string equivalent representation. It support International and Indian numbering system.</p>
+<p>**Note:** The Indian numbering system is used in the Indian subcontinent (Bangladesh, India, Nepal, Maldives, Pakistan and Sri Lanka) and in Burma.</p> 
+
+#### Number Range
+ * **International (default):** -999999999999999999999999999 to +999999999999999999999999999 
+ * **Indian:** -9999999999999 to +9999999999999
 
 ### Installation
 ```sh
@@ -8,15 +13,29 @@ $ npm install --save num-to-text
 
 ### Import Statement
 ```js
-import { toIndianCurrency } from "num-to-text";
+import { numToText } from 'num-to-text';
 ```
 
 ### Usage
 ```js
-let numericIndianCurrency = 1445;
+let number = 9231244534458;
+
+console.log("Number: " + 9231244534458)
+
 try {
-    let stringCurrency = toIndianCurrency(numericIndianCurrency);
-    console.log(stringCurrency);    
+    console.log("Default Format (International): " + numToText(number));
+} catch(e) {
+    console.error(e.message);
+}
+
+try {
+    console.log("International Format: " + numToText(number, 'int'));
+} catch(e) {
+    console.error(e.message);
+}
+
+try {
+    console.log("Indian Format: " + numToText(number, 'in'));
 } catch(e) {
     console.error(e.message);
 }
@@ -24,5 +43,8 @@ try {
 
 ### Output
 ```
-One Thousand Four Hundred Forty Five
+Number: 9231244534458
+Default Format (International): Nine Trillion Two Hundred Thirty One Billion Two Hundred Forty Four Million Five Hundred Thirty Four Thousand Four Hundred Fifty Eight
+International Format: Nine Trillion Two Hundred Thirty One Billion Two Hundred Forty Four Million Five Hundred Thirty Four Thousand Four Hundred Fifty Eight
+Indian Format: Ninety Two Kharab Thirty One Arab Twenty Four Crore Forty Five Lakh Thirty Four Thousand Four Hundred Fifty Eight
 ```
--- index.js
+++ index.js
@@ -37,15 +37,52 @@ const TENS = [
 const PLACES = [ 
     "",
     "",
-    "Hundred",
+    "Hundred"
+];
+
+const PLACES_INDIAN = [ 
+    "",
+    "",
+    "",
     "Thousand",
     "",
     "Lakh",
     "",
     "Crore",
-    ""
+    "",
+    "Arab",
+    "",
+    "Kharab"
 ];
-    
+
+const PLACES_INTERNATIONAL = [ 
+    "",
+    "",
+    "",
+    "Thousand",
+    "",
+    "",
+    "Million",
+    "",
+    "",
+    "Billion",
+    "",
+    "",
+    "Trillion",
+    "",
+    "",
+    "Quadrillion",
+    "",
+    "",
+    "Quintillion",
+    "",
+    "",
+    "Sextillion",
+    "",
+    "",
+    "Septillion"
+];
+
 function getTens(value) {
     value = parseInt(value);
     if(value > 99 || value < 1) return "";
@@ -61,32 +98,62 @@ function getHundred(value) {
     return (ONES[parseInt(value / 100)] + " " + PLACES[2] + " " + getTens(value % 100)).trim();
 }
 
-function getPositiveNumber(value) {
+function getPositiveIndianNumber(value) {
     value = parseInt(value);
     let result = getHundred(value % 1000);
     value = parseInt(value / 1000);
     if(value < 1) return result;
     let place = 3;
     while(value > 0) {
-        result = (getTens(value % 100) + " " + PLACES[place] + " " + result).trim();
+        result = (getTens(value % 100) + " " + PLACES_INDIAN[place] + " " + result).trim();
         value = parseInt(value / 100);
         place += 2;
     }
     return result;
 }
 
+function getPositiveInternationalNumber(value) {
+    value = parseInt(value);
+    let result = "";
+    if(value < 1) return result;
+    let place = 0;
+    while(value > 0) {
+        result = (getHundred(value % 1000) + " " + PLACES_INTERNATIONAL[place] + " " + result).trim();
+        value = parseInt(value / 1000);
+        place += 3;
+    }
+    return result;
+}
+
+function toIndian(value) {
+    if(Math.abs(value) > 9999999999999) throw new Error("Out of range number, currently it only supports indian numbering system from -9999999999999 to +9999999999999");
+    if(value == 0) return "Zero";
+    return (value < 0)? "Minus " + getPositiveIndianNumber(value * -1): getPositiveIndianNumber(value);
+}
+
+function toInternational(value) {    
+    if(Math.abs(value) > 999999999999999999999999999) throw new Error("Out of range number, currently it only supports indian numbering system from -999999999999999999999999999 to +9999999999999999999999999999999999999999");
+    if(value == 0) return "Zero";
+    return (value < 0)? "Minus " + getPositiveInternationalNumber(value * -1): getPositiveInternationalNumber(value);
+}
+
+
 /**
  * Converts any numeric Indian currency to string equivalent representation
  * @param value numeric Indian currency
  * @returns string equivalent currency
  */
-exports.toIndianCurrency = function(value) {
+function numToText(value, textFormat = 'int') {
     if(value) {
         if(value === "") throw new Error("Invalid number");
         value = parseInt(value);
-        if(Math.abs(value) > 999999999) throw new Error("Out of range number, currently it only support numbers from -999999999 to +999999999");
-        if(value == 0) return "Zero";
-        return (value < 0)? "Minus " + getPositiveNumber(value * -1): getPositiveNumber(value);    
+        switch(textFormat) {
+            case 'int': return toInternational(value);
+            case 'in': return toIndian(value);
+            default: throw new Error("Invalid text format");
+        }
     }
     throw new Error("Invalid number");
 }
+
+exports.numToText = numToText;
